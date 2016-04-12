77ba1ea633ac2218cdad9e03e9b7c52f3ed0273b
Danny Sauer <danny@dannysauer.com>
2016-04-11 21:19:40 -0500

    Start splitting in to separate client and server code

--- rainbow/rainbow_client.py
+++ rainbow/rainbow_client.py
@@ -4,16 +4,41 @@ import psycopg2
 import multiprocessing
 from string import letters,digits,printable
 from itertools import product
+#from Queue import Queue
 from threading import Thread
+
+salts = []
+#saltchars = 'ab'
+saltchars = digits
+#saltchars = letters + digits + './'
+for salt in product(saltchars, repeat=2):
+    salts.append( ''.join(salt) )
+
+def rainbow(password):
+    rs = []
+    for salt in salts:
+        #rs.append( {'p':password, 'h':salt + password} )
+        rs.append( {
+            'p':password,
+            'h':crypt.crypt(password, salt),
+            } )
+    return rs
 
 def generate_data(q, maxlen=2, minlen=1):
     """ create base passwords for consumer threads to crypt
     """
     alphabet = 'ab'
-    alphabet = printable
+    #alphabet = printable
     for l in range(minlen, maxlen+1):
         for s in product(alphabet, repeat=l):
             q.put( ''.join(s) )
+
+def consume_data(in_q, out_q):
+    """ pull data off of the queue to encrypt
+    """
+    while True:
+        out_q.put( rainbow( in_q.get() ))
+        in_q.task_done()
 
 def record_data(q):
     """ pull data from the queue and add to database
@@ -47,7 +72,12 @@ hashes    = multiprocessing.JoinableQueue(maxsize=0)
 
 # only one generator
 for i in range(1):
-    t = multiprocessing.Process( target=generate_data, args=(passwords, 5, 1))
+    t = multiprocessing.Process( target=generate_data, args=(passwords, 2, 1))
+    t.start()
+
+# however many consumers
+for i in range(3):
+    t = multiprocessing.Process( target=consume_data, args=(passwords, hashes))
     t.start()
 
 # only one data recorder?
