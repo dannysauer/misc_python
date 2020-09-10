1301a2963982a3a6e1cd330055ccc886fb471b60
Danny Sauer <dsauer@suse.com>
2020-09-10 15:39:50 -0500

    Reorganize code
    Attempting to make this look like this wasn't someone's very first program :D

--- misc/dockerstress.py
+++ misc/dockerstress.py
@@ -5,6 +5,47 @@ import docker
 import requests
 from time import sleep
 
+def main(target_instances):
+    client = docker.from_env()
+    containers = []
+
+    for i in range(1, target_instances + 1):
+        containers.append(create_container(client, i))
+
+    for cont in containers:
+        verify_container(cont)
+
+    for cont in containers:
+        print(f"Cleaning up {cont.name}")
+        cont.kill()
+        #cont.remove()
+
+def create_container(client, i):
+    host = f"stresshost_{i}"
+    print(f"creating {host}")
+    cont = client.containers.run(
+        "busybox",
+        ["/bin/httpd", "-fh", "/etc"],
+        hostname=host,
+        name=host,
+        remove=True,
+        detach=True,
+        ports={"80/tcp": None}, # picks a random port
+        publish_all_ports=True,
+        )
+    # have to get the container after it's started so things like
+    # "assigned ports" are fully populated.  Otherwise we just get
+    # the configuration value, which is None :/ 
+    return client.containers.get(cont.name)
+
+def verify_container(cont):
+    print(f"Verifying {cont.name}")
+    eport = cont.attrs['NetworkSettings']['Ports']['80/tcp'][0]['HostPort']
+    #print(f"Container {cont.name} exposes port {eport}")
+    url=f"http://localhost:{eport}/hostname"
+    r = requests.get(url)
+    print(f"{url} -> '{r.text.rstrip()}'")
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "number",
@@ -20,36 +61,5 @@ parser.add_argument(
     default="0",
     )
 args = parser.parse_args()
-target_instances = args.number
 
-client = docker.from_env()
-containers = []
-for i in range(1, target_instances + 1):
-    host = f"stresshost_{i}"
-    print(f"creating {host}")
-    cont = client.containers.run(
-        "busybox",
-        "/bin/httpd -fh /etc",
-        hostname=host,
-        name=host,
-        remove=True,
-        detach=True,
-        ports={"80/tcp": None}, # picks a random port
-        publish_all_ports=True,
-        )
-    # have to get the container after it's started so things like
-    # "assigned ports" are fully populated.  Otherwise we just get
-    # the configuration value, which is None :/ 
-    populated_cont = client.containers.get(cont.name)
-    containers.append(populated_cont)
-
-for cont in containers:
-    print(f"Verifying {cont.name}")
-    eport = cont.attrs['NetworkSettings']['Ports']['80/tcp'][0]['HostPort']
-    #print(f"Container {cont.name} exposes port {eport}")
-    url=f"http://localhost:{eport}/hostname"
-    r = requests.get(url)
-    print(f"{url} -> '{r.text.rstrip()}'")
-    print(f"Cleaning up {cont.name}")
-    cont.kill()
-    #cont.remove()
+main(args.number)
