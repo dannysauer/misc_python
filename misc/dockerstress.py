830cafa42a3ddf4d8f76b184687751f51599a788
Danny Sauer <dsauer@suse.com>
2020-09-10 15:03:26 -0500

    More functionality
    Open a random port for instance
    Instance count in a variable
    Print when instance is created and print before killing

--- misc/dockerstress.py
+++ misc/dockerstress.py
@@ -3,10 +3,13 @@
 import docker
 from time import sleep
 
+target_instances = 5
+
 client = docker.from_env()
 containers = []
-for i in range(1,50):
+for i in range(1, target_instances + 1):
     host = f"stresshost_{i}"
+    print(f"creating {host}")
     cont = client.containers.run(
         "busybox",
         "/bin/httpd -fh /etc",
@@ -14,10 +17,22 @@ for i in range(1,50):
         name=host,
         remove=True,
         detach=True,
+        ports={"80/tcp": None}, # picks a random port
+        publish_all_ports=True,
         )
-    containers.append(cont)
+    # have to get the container after it's started so things like
+    # "assigned ports" are fully populated.  Otherwise we just get
+    # the configuration value, which is None :/ 
+    populated_cont = client.containers.get(cont.name)
+    containers.append(populated_cont)
 
-sleep(5)
+#sleep(5)
+#import pprint
+#pp = pprint.PrettyPrinter(indent=4)
 for cont in containers:
+    #pp.pprint(cont.attrs)
+    populated_cont = client.containers.get(cont.name)
+    eport = cont.attrs['NetworkSettings']['Ports']['80/tcp'][0]['HostPort']
+    print(f"Container {cont.name} exposes port {eport}")
     cont.kill()
     #cont.remove()
