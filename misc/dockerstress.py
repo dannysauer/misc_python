6a7584d01fac39f66e334ae739c40907e9258fa9
Danny Sauer <dsauer@suse.com>
2020-09-10 15:15:19 -0500

    clean up comments

--- misc/dockerstress.py
+++ misc/dockerstress.py
@@ -27,11 +27,7 @@ for i in range(1, target_instances + 1):
     populated_cont = client.containers.get(cont.name)
     containers.append(populated_cont)
 
-#sleep(5)
-#import pprint
-#pp = pprint.PrettyPrinter(indent=4)
 for cont in containers:
-    #pp.pprint(cont.attrs)
     populated_cont = client.containers.get(cont.name)
     eport = cont.attrs['NetworkSettings']['Ports']['80/tcp'][0]['HostPort']
     #print(f"Container {cont.name} exposes port {eport}")
